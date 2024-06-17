654846e3c471b52c7e0df18993ac407a891d5f04
cisco <cisco@UPEAULA-31724>
2024-06-17 10:38:11 -0500

    Agregar archivos de proyecto.

---
+++ CruzSebastianTaller1P3/App.xaml.cs
@@ -0,0 +1,12 @@
+﻿namespace CruzSebastianTaller1P3
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new AppShell();
+        }
+    }
+}
---
+++ CruzSebastianTaller1P3/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace CruzSebastianTaller1P3
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
